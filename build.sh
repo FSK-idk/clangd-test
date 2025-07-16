101815a6dedca51a56e908b9c42a2d97fe011ada
fsk <vovarybkin666@gmail.com>
2025-07-16 18:06:56 +0300

    clean up, still reproducible

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+rm -rf .cache bin build
 cmake --preset clangd-test
 cmake --build --preset clangd-test
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@ import foo;
 import bar;
 
 auto main() -> int {
-    test::i32 somevar;
     std::println("{}", test::myvar);
     return 0;
 }
